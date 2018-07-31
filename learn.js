eb52e82f87eff7d89aaf0c286d8869e8b366f43b
maabed <veto44@gmail.com>
2018-07-31 11:59:00 +0300

    init, dont trust first commit

---
+++ learn.js
@@ -0,0 +1,82 @@
+
+let name = 'Mohamed';
+let condition = true; // false
+let undef = undefined;
+let array = ['Sara', '22', false, undef]
+let obj = {
+  name: 'Salah',
+  age: 25,
+}
+
+// || or
+// && and
+// ! not
+// !!
+// ===
+// !==
+
+
+// let age = 30;
+
+// if (typeof age === 'number' && age < 26) { // false // || or 
+//   document.write('Yes age is less than 26' + '<br>')
+// } else if (age === 30) { // false
+//   document.write('Yes age is greater than 30' + '<br>')
+// } else {
+//   document.write('Yes age is not greater than 30 or less than 26' + '<br>')
+// }
+
+// if (true) {
+//   alert('Thats true');
+// } else if (true) {
+//   // code
+// } else if (false) {
+//  // code
+// } else {
+//  // code
+// }
+
+
+// let num = 50;
+// switch(num) {
+//   case 50:
+//     document.write('Number is 50' + '<br>');
+//     break;
+//   case 60:
+//     document.write('Number is 60 || 80' + '<br>');
+//     break;
+//   case 80:
+//     document.write('Number is 60 || 80' + '<br>');
+//     break;
+//   default:
+//     return false;
+// }
+
+// coce
+
+
+// let sum = 0;
+// for (let i = 0; i < 10; i++) {
+//   // document.write(i + '<br>');
+//   sum += i;
+//   console.log('In loop sum');
+//   console.log(sum);
+// }
+
+// document.write(sum + '<br>');
+
+
+let student = {
+  name: 'Ahmad',
+  age: 25,
+  adderss: '123 AMM'
+}
+let arr = [11, 4, 5, 100, 2, 22, 45];
+
+for(index in arr) {
+  console.log(index);
+  console.log(arr[index]);
+}
+
+
+
