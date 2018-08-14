7c8a5c5eb13375e390e4da84ddd48b85dec7374f
maabed <veto44@gmail.com>
2018-08-14 11:10:34 +0300

    more es6 stuff

--- learn.js
+++ learn.js
@@ -25,19 +25,25 @@ let obj = {
 
 
 // ==================== forEach ======================== //
-let numbers = [5, 7, 1, 9, 8, 5];
+// let numbers = [5, 7, 1, 9, 8, 5];
+// let oddNumbers = [];
+// let eventNumbers = [];
  
-numbers.forEach((value, idx) => {
-  if (value % 2) {
-    console.log('odd');
-  } else {
-    console.log('even');
-  }
-});
+// let results = numbers.forEach((value, idx) => {
+//   console.log(value)
+//   if (value % 2) {
+//     oddNumbers.push(value);
+//   } else {
+//     eventNumbers.push(value);
+//   }
+// });
 
 // ==================== map ======================== //
 // const numbers = [2, 4, 8, 10];
-// const halves = numbers.map(x => x / 2);
+// const halves = numbers.map((x, idx) => {
+//   return x / 2;
+// });
+// console.log(halves);
 
 // ================== map vs forEach ===================== //
 // The main difference that you need to know is .map() returns a new array while .forEach() doesn't. That is why you see that difference in the output. .forEach() just operates on every value in the array.
@@ -46,7 +52,7 @@ numbers.forEach((value, idx) => {
 // arr.forEach((num, index) => {
 //   return arr[index] = num * 2;
 // });
-// // arr = [2, 4, 6, 8, 10]
+// arr = [2, 4, 6, 8, 10]
 // let doubled = arr.map(num => {
 //   return num * 2;
 // });
@@ -57,8 +63,11 @@ numbers.forEach((value, idx) => {
 // halves is [1, 2, 4, 5]
 // const words = ["spray", "limit", "elite", "exuberant", "destruction", "present"];
 
-// const longWords = words.filter(word => word.length > 6);
+// const longWords = words.filter((word) => { 
+//   return word.length < 6
+// });
 // // longWords is ["exuberant", "destruction", "present"]
+// console.log(longWords)
 
 let data = [
   {
@@ -84,43 +93,48 @@ let data = [
 ];
 
 // ==================== reduce ======================== //
+
+
 // const total = [0, 1, 2, 3].reduce((sum, value) => sum + value, 1);
-// let objResult = data.reduce((a, i) =>  a + i.age, 0);
+// let objResult = data.reduce((sum, i) => sum + i.age, 0);
 // console.log(objResult/data.length);
 
-// let numArry = [1, 4, 6, 8];
-// let result = numArry.reduce((a, i) =>  a + i);
-// console.log(result);
+// let numArry = [1, 4, 6, 8];  // sum = 11 +8 = 19
+// let result = numArry.reduce((sum, value) =>  sum + value);
+// console.log(result/numArry.length);
 
 // ===================== every =======================//
+
 // [12, 5, 8, 130, 44].every(x => x >= 10); // false
 // [12, 54, 18, 130, 44].every(x => x >= 10); // true
 
-
-// testing merge conflicts 100
-// testing merge conflicts 105
 
 // ==================== findIndex ========================//
 
 // const fruits = ["apple", "banana", "cantaloupe", "blueberries", "grapefruit"];
 
-// const index = fruits.findIndex(fruit => fruit === "blueberries");
+// const index = fruits.findIndex(x => x === "blueberries");
 
 // console.log(index); // 3
-// console.log(fruits[index]); // blueberries
+// console.log(fruits[3]); // blueberries
 
 // ====================== Chaines ======================//  
 
 // let arr = [1, 2, 3, 4, 5];
-// let arr2 = arr.map(num => num * 2).filter(num => num > 5);
+// let arr2 = arr.map(num => num * 2)
+//               .filter(num => num > 5)
+//               .findIndex((x) =>  x === 8);
+
+// console.log(arr2);
+
+
 // arr2 = [6, 8, 10]
 
 // Note
 // let numArry = [1, 4, 6, 8];
 // let total = 0
-// for (let i = 0; i < numArry.length; total += numArry[i++]);
-// console.log(total)
 
+// for (let i = 0; i < numArry.length; total += numArry[i++]);
 
 
 // testing merge conflicts 
